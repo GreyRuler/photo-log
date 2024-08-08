6ae6e5f8221d5e428c1706051534ce5451466827
GreyRuler <daniil_vasilenko757@mail.ru>
2024-08-08 11:49:36 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
 import path from "path"
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import laravel from 'laravel-vite-plugin';
 import viteReact from "@vitejs/plugin-react";
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      laravel({
-          input: 'src/main.tsx',
-          refresh: true,
-      }),
-      viteReact(),
-      TanStackRouterVite()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        laravel({
+            input: 'src/main.tsx',
+            refresh: true,
+        }),
+        viteReact(),
+        TanStackRouterVite()
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    server: {
+        host: true,
+        port: 3000,
+    }
 });
